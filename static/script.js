4e709fae2f23e3d5058555b6a9c3494bb3534d56
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-06-16 00:31:32 +0800

    added modal

---
+++ static/script.js
@@ -0,0 +1,8 @@
+alert(1);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const faqModal = document.querySelector(".faq-button")
+    faqModal.addEventListener("click", () => {
+        console.log(clicked)
+    })
+})
